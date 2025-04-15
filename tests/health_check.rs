1c9c337a6571fe25efd5dda7e9e4c3b5a972c3a3
legacyofmu <legacyofmu1@gmail.com>
2025-04-15 11:28:26 +0900

    Change test choosing a random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,15 +6,15 @@
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    //spawn_app().await.expect("Failed to spawn our app.");
-    spawn_app();// No .await, no .expect
-
+    let address = spawn_app();// No .await, no .expect
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -23,9 +23,13 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
 
 
